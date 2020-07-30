693134fb70dc24ad1c1cebf9660204a047068c33
David Reke <david.e.reke@gmail.com>
2020-07-30 09:19:36 -0400

    Updates

---
+++ precache-manifest.ce3484cef06db6f73a4db616f45c4ee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef0ce7228d95cad103ecc4287c87f4a",
+    "url": "/io/secondReactLectureJuly20/index.html"
+  },
+  {
+    "revision": "511b05d124f5b99cbed3",
+    "url": "/io/secondReactLectureJuly20/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "56279776a2d047bda46c",
+    "url": "/io/secondReactLectureJuly20/static/js/2.7af59a0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/io/secondReactLectureJuly20/static/js/2.7af59a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "511b05d124f5b99cbed3",
+    "url": "/io/secondReactLectureJuly20/static/js/main.8af5bd0b.chunk.js"
+  },
+  {
+    "revision": "e0fde2e968d8efc37481",
+    "url": "/io/secondReactLectureJuly20/static/js/runtime-main.8f13ddcc.js"
+  }
+]);
